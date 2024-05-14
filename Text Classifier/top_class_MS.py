aaab70c39c55a2debbc79e403931d78af6ef256d
Michael <michael.sowter@ofcom.org.uk>
2024-05-14 14:14:38 +0000

    added Output data folder, started inferencing for jupyter file, testing if running models in .py file stops training from ending early

---
+++ Text Classifier/top_class_MS.py
@@ -0,0 +1,184 @@
+# MS: create training set, evaluation set and validation set
+import sys
+import pandas as pd
+import csv
+
+dataset_name = "/home/azureuser/cloudfiles/code/Users/Omololu.Makinde/Llama_tutorial/data/consultation2.csv"
+dataset = []
+
+with open(dataset_name, encoding='utf-8-sig') as FID:
+    csvReader = csv.DictReader(FID, delimiter="\t")
+    for key, row in enumerate(csvReader): 
+        # print(key, row)
+        dataset.append({
+            "label" : row['Topic'].strip().lower().replace('\n', ''),
+            "text" : row['Full summary of comment'],
+            "summary" : row['One-line summary']
+        })
+
+df_dataset = pd.DataFrame(dataset)
+df_dataset.drop(columns =['summary'], inplace=True)  # don't need this for now
+# display(df_dataset)
+
+
+
+df_dataset_count = df_dataset['label'].value_counts()
+df_dataset['label'].value_counts()
+
+
+
+
+keep_rows = list(df_dataset_count[df_dataset_count > 10].index)
+df_dataset = df_dataset[df_dataset['label'].isin(keep_rows)]
+# display(df_dataset['label'].value_counts())
+
+
+
+
+import numpy as np
+
+# add in our classifier labels
+topic = 'governance and accountability'
+df_dataset['label'] = np.where(np.array(df_dataset['label']) == topic, 1, 0)
+df_dataset['label'].value_counts(normalize=False)
+
+
+
+# train and test data
+class_len = len(df_dataset[df_dataset['label'] == 1])  # find how many values we can take and still have a balanced class
+class_0_data = df_dataset[df_dataset.label.eq(0)].sample(class_len) 
+class_1_data = df_dataset[df_dataset.label.eq(1)].sample(class_len)
+train_test_data = pd.concat([class_0_data, class_1_data])  # 50/50 class split
+# display(train_test_data)
+
+# evaluation data
+eval_data = df_dataset.drop(train_test_data.index)  # put the rest into an evaluation set we can play with
+eval_data
+
+
+
+from datasets import Dataset
+from transformers import AutoTokenizer
+
+huggingface_data = Dataset.from_pandas(train_test_data, preserve_index=False)  # don't include pandas index
+
+pretrained_model_name = "distilbert/distilbert-base-uncased"  # This is our base model
+
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
+def proc_data(data):
+    return tokenizer(data['text'], max_length=512, padding=True, truncation=True)
+
+tokenized_data = huggingface_data.map(proc_data, batched=True)  # advantage of ".map" is we can parallel process data in batches (i think)
+# print(tokenized_data['text'] == huggingface_data['text'])  # SHOULDN'T THIS NOT BE TRUE??? ESPECIALLY IF I CHANGE MAX_LENGTH TO BE 1 OR SOMETHING
+
+split_tokenized_hugginface_data = tokenized_data.train_test_split(test_size=0.10)  # 85/15 train/test split
+print(split_tokenized_hugginface_data)
+
+
+
+import os
+import shutil
+
+def clear_folder_except(folder_path, exclude_folder):
+    # Iterate over the items in the directory
+    for item_name in os.listdir(folder_path):
+        item_path = os.path.join(folder_path, item_name)
+        # Skip the exclude folder
+        if item_name == exclude_folder:
+            continue
+        # Check if the item is a directory
+        if os.path.isdir(item_path):
+            # If it is a directory, remove it and its contents
+            shutil.rmtree(item_path)
+        else:
+            # If it is a file, remove it
+            os.remove(item_path)
+
+
+
+
+# -HOT FIX TO STOP MODEL TRAINING BREAKING----------------------------------------------------------------------------------------------------------- #
+model_output_path = "/home/azureuser/cloudfiles/code/Users/Michael.Sowter/Deep_Learning_Training/Text Classifier/Models/Mod_1"
+# clear_folder_except(model_output_path, "runs")
+# --------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer, EarlyStoppingCallback
+from transformers import DataCollatorWithPadding
+import evaluate
+
+
+# Select accuracy metric
+evaluation_metrics = ["accuracy", "f1", "precision", "recall"]
+accuracy = evaluate.combine(evaluation_metrics)# evaluate.load("accuracy")
+
+# Use accuracy to determine which class is the most likely prediction
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred  # get predictions and labels
+    predictions = np.argmax(predictions, axis=1)  # calculate highest probability class (negative or positive sentiment)
+    return accuracy.compute(predictions=predictions, references=labels)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+id2label = {0: "Negative", 1: "Positive"}
+label2id = {"Negative": 0, "Positive": 1}
+
+
+model = AutoModelForSequenceClassification.from_pretrained(pretrained_model_name,
+                                                            id2label=id2label,
+                                                            label2id=label2id)
+
+# How we input training arguments into the model
+
+training_args = TrainingArguments(
+    output_dir=model_output_path,  # path model stored
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=20,
+    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=False,
+    logging_steps= 25 # logs made every X batches. So smaller log means more information recorded (see log in table but also more computational and memory requirements)
+    )
+
+
+# Model training arguments
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=split_tokenized_hugginface_data["train"],#.select(range(200)),
+    eval_dataset=split_tokenized_hugginface_data["test"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+    # callbacks=[EarlyStoppingCallback(early_stopping_patience=5)]  # Stop training if no improvement after 3 consecutive epochs
+
+)
+
+# Train model
+trainer.train()
+
+# Save the model
+trainer.save_model("/home/azureuser/cloudfiles/code/Users/Michael.Sowter/Deep_Learning_Training/Text Classifier/Models/Mod_1/Best")
+
+
+
+# Parse the pdf
+from tika import parser
+
+pdf_filepath = "/home/azureuser/cloudfiles/code/Users/Michael.Sowter/Deep_Learning_Training/Overview.pdf"
+parsed_file = parser.from_file(pdf_filepath)['content']
+print(parsed_file)
+
+
+
+# Split pdf into chunks
+from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.embeddings import HuggingFaceEmbeddings
+
+embedding = HuggingFaceEmbeddings(model_name="avsolatorio/GIST-small-Embedding-v0")  # get embeddings model
+text_splitter = SemanticChunker(embedding)  # apply the model to the type of split we want to perform (a semantic split)
+text_splitter.create_documents([parsed_file])
